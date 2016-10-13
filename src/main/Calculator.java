3e9cb13f95e95258a7adc9d658ca22fe58d0c8af
Njáll Hilmar Hilmarsson <gitlab@deadbyte.is>
2016-10-13 10:30:28 +0000

    Correctly implemented addition of 2 and 3 numbers that are comma seperated

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -2,8 +2,15 @@ package stringcalculator;
 
 public class Calculator {
     public static int add(String text) {
-        if(text == "")
+        if(text.equals(""))
             return 0;
-        return Integer.parseInt(text);
+        String[] numbers = text.split(",");
+        if(numbers.length == 1)
+            return Integer.parseInt(numbers[0]);
+        else if(numbers.length == 2)
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        else if(numbers.length == 3)
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]) + Integer.parseInt(numbers[2]);
+        return 0;
     }
 }
