7327947cf9f322696e2b4e89a0d0b04a5f3ec93f
Njáll Hilmar Hilmarsson <gitlab@deadbyte.is>
2016-10-13 10:56:07 +0000

    Correctly implemented raising exceptions for negative numbers in all comma and newline seperated strings

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -5,12 +5,21 @@ public class Calculator {
         if(text.equals(""))
             return 0;
         String[] numbers = text.replace("\n", ",").split(",");
+        String errstring = "Negatives not allowed: ";
+        int errlength = errstring.length();
         int comb = 0, tmp;
         for(int i = 0; i < numbers.length; i++) {
             tmp = Integer.parseInt(numbers[i]);
-            if(tmp <= 1000)
+            if(tmp <= 1000 && tmp >= 0)
                 comb += tmp;
+            else if(tmp < 0) {
+                if(errstring.length() != errlength)
+                    errstring += ",";
+                errstring += tmp;
+            }
         }
+        if(errstring.length() > errlength)
+            throw new IllegalArgumentException(errstring);
         return comb;
     }
 }
