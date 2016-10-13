894059d919d08b97675bd88fb8c14170199f4947
Njáll Hilmar Hilmarsson <gitlab@deadbyte.is>
2016-10-13 10:32:36 +0000

    Correctly implemented addition of all numbers that are comma seperated

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -5,12 +5,10 @@ public class Calculator {
         if(text.equals(""))
             return 0;
         String[] numbers = text.split(",");
-        if(numbers.length == 1)
-            return Integer.parseInt(numbers[0]);
-        else if(numbers.length == 2)
-            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
-        else if(numbers.length == 3)
-            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]) + Integer.parseInt(numbers[2]);
-        return 0;
+        int comb = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            comb += Integer.parseInt(numbers[i]);
+        }
+        return comb;
     }
 }
