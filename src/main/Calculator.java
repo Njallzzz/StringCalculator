468e50ba64cfb05fb887afa0295cb7eda0c20b03
Njáll Hilmar Hilmarsson <gitlab@deadbyte.is>
2016-10-13 11:12:13 +0000

    Correctly implemented semicolumn seperation, all test cases are satisfied

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -4,18 +4,24 @@ public class Calculator {
     public static int add(String text) {
         if(text.equals(""))
             return 0;
-        String[] numbers = text.replace("\n", ",").split(",");
+        String[] numbers;
+        if(text.contains(";"))
+            numbers = text.replace("\n", "").replace(",", "").replace("\\", "").split(";");
+        else
+            numbers = text.replace("\n", ",").split(",");
         String errstring = "Negatives not allowed: ";
         int errlength = errstring.length();
         int comb = 0, tmp;
         for(int i = 0; i < numbers.length; i++) {
-            tmp = Integer.parseInt(numbers[i]);
-            if(tmp < 0) {
-                if(errstring.length() != errlength)
-                    errstring += ",";
-                errstring += tmp;
-            } else if(tmp <= 1000)
-                comb += tmp;
+            if(!numbers[i].equals("")) {
+                tmp = Integer.parseInt(numbers[i]);
+                if (tmp < 0) {
+                    if (errstring.length() != errlength)
+                        errstring += ",";
+                    errstring += tmp;
+                } else if (tmp <= 1000)
+                    comb += tmp;
+            }
         }
         if(errstring.length() > errlength)
             throw new IllegalArgumentException(errstring);
